885a3695d03509a30cee97332f852b817800af4c
flyingalexis <mkhtang@connect.ust.hk>
2018-09-24 17:24:17 +0800

    update optimizer

--- models/params_optimizer.py
+++ models/params_optimizer.py
@@ -9,10 +9,16 @@
 #       'concession': 0.02  # error within 2%
 #     }
 import decimal
+import itertools
 
-def cnn_optimize(){
+def cnn_optimize():
     lr = [decimal.Decimal(i) / decimal.Decimal(10) for i in range(0, 3)]
-    filter_num = list(range(10,200))
+    filter_num = list(range(50,100))
     layers_num = list(range(1,5))
-    
-}
+    # filter_dim = [ list(itertools.permutations(filter_num, i)) for i in layers_num ]
+    print(list(itertools.permutations(filter_num, 5)))
+    # print(filter_dim)
+    # print(list(itertools.product(lr,filter_num,layers_num)))
+
+
+cnn_optimize()
